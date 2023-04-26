7bfbc80b60116c7e5f1d46d3938c4ea0fb58e5f0
Sam <enquiry.conaticus@gmail.com>
2023-04-26 17:26:10 +0100

    add gif support & more secure file validation

--- router/cdn.go
+++ router/cdn.go
@@ -1,14 +1,11 @@
 package router
 
 import (
-	"crypto/md5"
-	"io/ioutil"
-	"net/http"
-	"strings"
-
-	. "cdn/api/util"
-
 	"cdn/api/database"
+	. "cdn/api/util"
+	"crypto/md5"
+	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -19,31 +16,52 @@ func uploadEndpoint(c *gin.Context) {
 
 	fileHeader, err := c.FormFile(paramName)
 	if err != nil {
-		c.String(http.StatusForbidden, "Invalid file upload, parameter must be named '%s'.", paramName)
+		c.String(http.StatusBadRequest, "Invalid file upload: %s", err.Error())
 		return
 	}
 
-	fileType := fileHeader.Header.Get("Content-Type")
-	if !strings.HasPrefix(fileType, "image/") {
-		c.String(http.StatusForbidden, "Invalid file upload, file type must be an image. Found '%s'.", fileType)
+	file, err := fileHeader.Open()
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to open uploaded file: %s", err.Error())
+		return
 	}
-
-	file, _ := fileHeader.Open()
 	defer file.Close()
 
-	fileBuffer, _ := ioutil.ReadAll(file)
+	// Validate file type based on file content
+	fileBuffer := make([]byte, 512) // Read first 512 bytes of file content
+	_, err = file.Read(fileBuffer)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Failed to read file: %s", err.Error())
+		return
+	}
+	fileType := http.DetectContentType(fileBuffer)
+
+	// Check if file type is allowed (image MIME types whitelist)
+	allowedMIMETypes := map[string]bool{
+		"image/jpg":  true,
+		"image/jpeg": true,
+		"image/png":  true,
+		"image/gif":  true,
+	}
+	if !allowedMIMETypes[fileType] {
+		c.String(http.StatusBadRequest, "Invalid file upload: file type must be an image. Found '%s'.", fileType)
+		return
+	}
+
+	// Upload the file if has not already been uploaded
 	fileHashBuffer := md5.Sum(fileBuffer)
-
-	fileName := uuid.NewString() + "." + GetFileExtension(fileHeader.Filename)
-
+	fileName := uuid.NewString() + filepath.Ext(fileHeader.Filename)
 	savedFileName, alreadyExists := database.AddImage(fileName, fileHashBuffer[:])
 	if !alreadyExists {
-		c.SaveUploadedFile(fileHeader, "./uploads/images/" + fileName)
+		err = c.SaveUploadedFile(fileHeader, "./uploads/images/"+fileName)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Failed to save uploaded file: %s", err.Error())
+			return
+		}
 	}
 
 	body := gin.H{
 		"file_url": Config.HostUrl + "download/" + savedFileName,
 	}
-
 	c.JSON(http.StatusOK, body)
 }
--- util/files.go
+++
@@ -1,9 +0,0 @@
-package util
-
-import "strings"
-
-// Get file extension from a file
-func GetFileExtension(filename string) string {
-	split := strings.Split(filename, ".")
-	return split[len(split) - 1]
-}
